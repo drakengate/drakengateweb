ab91c16fe96d09cad3266cc6b73044bcc79bcf7c
developer <samsonjohnn@yandex.com>
2023-10-15 22:12:37 +0300

    title

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript,Title } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Drakengate</title>
       <body>
         <Main />
         <NextScript />
