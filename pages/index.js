5b1cdc89c95022be5af4dc994c53761e28d0e780
developer <samsonjohnn@yandex.com>
2023-10-15 21:57:52 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="flex md:flex-row  flex-col justify-center md:justify-between w-full">
        
         <div className="text-white flex  p-12 text-3xl cursor-pointer  ">
-          <Link href="/presale">BUY</Link>
+          <Link href="presale.drakengate.com">BUY</Link>
         </div>
         <div className="flex flex-col items-center ">
           <Image src="/logodragontwo.png" width={180} height={180} />
