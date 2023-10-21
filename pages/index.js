e0c764bb66a6d7a91fab8aa8a5a020b580a9a48f
developer <samsonjohnn@yandex.com>
2023-10-21 17:11:32 +0300

    whitepaper and x added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Young_Serif, Tangerine } from "next/font/google";
 import Link from "next/link";
 import { BsTelegram } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
 
 const young = Young_Serif({ weight: "400", subsets: ["latin"] });
 const tangerine = Tangerine({ weight: "400", subsets: ["latin"] });
@@ -12,18 +13,23 @@ export default function Home() {
       className={`flex  flex-col items-center bg-transparent justify-between uppercase tracking-widest `}
     >
       <div className="flex md:flex-row   justify-center md:justify-between w-full">
-       
         <div className="text-white flex justify-center p-4  md:p-12 text-3xl cursor-pointer  ">
-          <Link className="md:pl-24" passHref={true} href="https://presale.drakengate.com">BUY</Link>
-          
+          <Link
+            className="md:pl-24"
+            passHref={true}
+            href="https://presale.drakengate.com"
+          >
+            BUY
+          </Link>
         </div>
         <div className="flex flex-col justify-center items-center ">
           <Image src="/logo.png" width={180} height={180} />
           <p className="text-3xl text-white mt-4"> Drakengate </p>
         </div>
         <div className="text-white flex justify-center p-4  md:p-12 text-3xl cursor-pointer  ">
-         
-          <Link className="md:pr-24" passHref={true} href="/story">read</Link>
+          <Link className="md:pr-24" passHref={true} href="/story">
+            read
+          </Link>
         </div>
       </div>
 
@@ -44,24 +50,42 @@ export default function Home() {
           <p className="text-5xl text-white"></p>
         </div>
         <div className="flex justify-center mt-12 flex-col items-center">
-        <p className="text-3xl text-white  ">
-          Join Now
-        </p>
-        
-        <Link href="https://t.me/drakengate" >
-          <BsTelegram
-            className="text-white  mt-2  cursor-pointer"
-            size={40}
-          ></BsTelegram>
-        </Link>
-        <audio controls className="mt-8">
-          <source src="Story.mp3" type="audio/mpeg"/>
-        </audio>
-        <p className="text-white mt-12">Total Supply: 204,242,424,242,424</p>
-        <p className="text-white mt-8">Launch Date: Community will decide</p>
-        <div className="text-white text-2xl mt-8">
-          <Link className="underline" target="_blank" href="https://forms.gle/NPiS15wcCzedhbPT9">Join our Team</Link>
-        </div>
+          <p className="text-3xl text-white pb-6">
+            <Link target="_blank" href="https://whitepaper.drakengate.com">
+              Whitepaper
+            </Link>
+          </p>
+
+          <p className="text-3xl text-white  ">Join Now</p>
+
+          <div className="flex gap-2">
+            <Link target="_blank" href="https://t.me/drakengate">
+              <BsTelegram
+                className="text-white  mt-2  cursor-pointer"
+                size={40}
+              ></BsTelegram>
+            </Link>
+            <Link target="_blank" href="https://twitter.com/DrakenGateETH">
+              <FaXTwitter
+                className="text-white  mt-2  cursor-pointer"
+                size={40}
+              ></FaXTwitter>
+            </Link>
+          </div>
+          <audio controls className="mt-8">
+            <source src="Story.mp3" type="audio/mpeg" />
+          </audio>
+          <p className="text-white mt-12">Total Supply: 204,242,424,242,424</p>
+          <p className="text-white mt-8">Launch Date: Community will decide</p>
+          <div className="text-white text-2xl mt-8">
+            <Link
+              className="underline"
+              target="_blank"
+              href="https://forms.gle/NPiS15wcCzedhbPT9"
+            >
+              Join our Team
+            </Link>
+          </div>
         </div>
       </div>
     </main>
